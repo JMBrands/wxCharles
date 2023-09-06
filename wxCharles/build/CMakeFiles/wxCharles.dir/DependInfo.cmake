2635fa45154e749cb6c2cf65e3a0849f8e2fb414
SypTitan <sybrentitan@gmail.com>
2023-09-06 15:31:32 +0200

    Yey charles is on the hub
    Signed-off-by: SypTitan <sybrentitan@gmail.com>

---
+++ wxCharles/build/CMakeFiles/wxCharles.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/res/charles.rc" "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/build/CMakeFiles/wxCharles.dir/framework/res/charles.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "UNICODE"
+  "WXUSINGDLL"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "C:/Users/11486/AppData/Local/Programs/ImperativeProgramming/wxWidgets-3.2.2.1/lib/gcc_dll/mswu"
+  "C:/Users/11486/AppData/Local/Programs/ImperativeProgramming/wxWidgets-3.2.2.1/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/agent.cpp" "CMakeFiles/wxCharles.dir/agent.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/agent.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/app.cpp" "CMakeFiles/wxCharles.dir/framework/app.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/app.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/assignment.cpp" "CMakeFiles/wxCharles.dir/framework/assignment.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/assignment.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/charles.cpp" "CMakeFiles/wxCharles.dir/framework/charles.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/charles.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/freeplay_window.cpp" "CMakeFiles/wxCharles.dir/framework/freeplay_window.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/freeplay_window.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/scenario.cpp" "CMakeFiles/wxCharles.dir/framework/scenario.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/scenario.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/simulation_window.cpp" "CMakeFiles/wxCharles.dir/framework/simulation_window.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/simulation_window.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/trace.cpp" "CMakeFiles/wxCharles.dir/framework/trace.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/trace.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/trace_list_control.cpp" "CMakeFiles/wxCharles.dir/framework/trace_list_control.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/trace_list_control.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/world.cpp" "CMakeFiles/wxCharles.dir/framework/world.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/world.cpp.obj.d"
+  "C:/Users/11486/OneDrive/Documenten/@Programming/@C++/wxCharles-demo/wxCharles/framework/world_control.cpp" "CMakeFiles/wxCharles.dir/framework/world_control.cpp.obj" "gcc" "CMakeFiles/wxCharles.dir/framework/world_control.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
