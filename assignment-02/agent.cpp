fa075b4f4281811e4be1afa033e4470a190f3233
SypTitan <sybrentitan@gmail.com>
2023-09-12 10:26:24 +0200

    Added week 2's assignment

---
+++ assignment-02/agent.cpp
@@ -0,0 +1,23 @@
+#include "framework/charles.h"
+#include "assignments/assignment2.h"
+
+void test_agent() {
+
+}
+
+void path_agent() {
+
+}
+
+// NOTE: this agent is executed twice in a row. See the PDF for more details.
+void cave_agent() {
+
+}
+
+void find_center_agent() {
+
+}
+
+void clean_up_agent() {
+
+}
