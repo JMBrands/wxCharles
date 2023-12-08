e918c496ff2e8adfab2c22972df35131fdfbd831
SypTitan <sybrentitan@gmail.com>
2023-12-08 23:52:22 +0100

    Finished main for mandatory 12

--- assignment-12-mandatory/main.cpp
+++ assignment-12-mandatory/main.cpp
@@ -12,8 +12,6 @@ struct Gift {
     string name;
 };
 
-typedef vector<Gift> Inventory;
-
 struct Store {
     vector<Gift> inventory;
 };
@@ -28,12 +26,16 @@ istream& operator>> (istream& in, Gift& gift) {
     string name;
     in >> price;
     getline(in, name);
+    if (name.at(0) == ' ') {
+        name = name.substr(1);
+    }
     gift = {price, name};
     return in;
 }
 
 ostream& operator<< (ostream& out, const Gift& gift) {
-    out  << static_cast<float>(gift.price)/100 << " euros, " << gift.name;
+    out  << static_cast<float>(gift.price)/100 << " euros, ";
+    out << gift.name;
     return out;
 }
 
@@ -49,16 +51,25 @@ bool operator== (const Gift& a, const Gift& b) {
 }
 
 void read_store(istream& store, Store& giftstore) {
+// precondition:
+    assert(!store.eof());
+/* post-condition:
+    giftstore gets filled with all the items in store
+*/
     Gift gift;
-    while (!store.eof()) {
-        store >> gift; // TODO: make this stop putting random values at the end
-        cout << gift << endl;
+    while (store.peek() != EOF) {
+        store >> gift;
         giftstore.inventory.push_back(gift);
     }
-    giftstore.inventory.pop_back();
 }
 
 Gift find_gift(string giftname, const Store& store) {
+// precondition:
+    assert(true);
+/* post-condition:
+    if a gift with the name giftname is in store,
+    it will get returned, otherwise return an empty gift
+*/
     for (Gift gift : store.inventory) {
         if (gift.name == giftname) {
             return gift;
@@ -67,13 +78,18 @@ Gift find_gift(string giftname, const Store& store) {
     return {};
 }
 
-void read_available_wishlist(istream& list, Wishlist& wishlist, const Store& store) {
+void read_available_wishlist(istream& list, Wishlist& wishlist, const Store& store) {    
+// precondition:
+    assert(!list.eof());
+/* post-condition:
+    wishlist gets filled with every item in store that's also on list
+*/
     list >> wishlist.budget;
     string giftname;
     Gift gift;
     Gift not_found = {};
     while (!list.eof()) {
-        list >> giftname;
+        getline(list, giftname);
         gift = find_gift(giftname, store);
         if (gift != not_found) {
             wishlist.gifts.push_back(gift);
@@ -81,22 +97,90 @@ void read_available_wishlist(istream& list, Wishlist& wishlist, const Store& sto
     }
 }
 
-void show(Wishlist& list, cents remaining) {
+void show(const Wishlist& list, cents remaining) {
+// precondition:
+    assert(true);
+/* post-condition:
+    prints the entire wishlist, and the budget remaining on it
+*/
     for (Gift gift : list.gifts) {
-        cout << gift << endl;
+        cout << gift;
+        cout << endl;
     }
     cout << "Budget remaining: " << remaining << " cents" << endl;
 }
 
-cents gifts(const Wishlist& list, Wishlist& current, int index, cents budget) {
-    
+cents gifts(const Wishlist& list, Wishlist& current, int index, cents budget, Wishlist& optimal) {
+// precondition:
+    assert(true);
+/* post-condition:
+    optimal is the list with the least amount of budget remaining, 
+    made from only items on list
+*/
+
     if (index >= ssize(list.gifts)) {
-        
-        return budget;
+        return -1;
     }
-
-
+    if (budget < 1) {
+        return -1;
+    }
+    current.gifts.push_back(list.gifts.at(index));
+    int budget_with = gifts(list, current, index+1, budget-list.gifts.at(index).price, optimal);
+    current.gifts.pop_back();
+    int budget_without = gifts(list, current, index+1, budget, optimal);
+    
+    if (budget_with == -1 && budget_without == -1 && budget < list.budget) {
+        if (budget < optimal.budget) {
+            //show(current, budget);
+            //cout << "optimal budget: " << static_cast<int>(optimal.budget) << endl;
+            //cout << endl;
+            optimal.budget = budget;
+            optimal.gifts = current.gifts;
+        }
+    }
+    
+    if (budget_with == -1) {
+        if (budget_without != -1) {
+            budget = budget_without;
+        }
+    }
+    else if (budget_without == -1) {
+        budget = budget_with;
+    }
+    else {
+        budget = min(budget_with, budget_without);
+    }
+    
     return budget;
+}
+
+void calculate_wishlist(string person, string storename, ofstream& out) {
+//precondition:
+    assert(true);
+/* post-condition:
+    prints the optimal gift selection for the person,
+    when bought at storename. Also sends it to the specified file.
+*/
+    
+    ifstream store(storename+".txt");
+    Store giftstore;
+    read_store(store,giftstore);
+    ifstream Person(person+".txt");
+    Wishlist list, cur, solution;
+    read_available_wishlist(Person, list, giftstore);
+    solution.budget = list.budget;
+    gifts(list, cur, 0, list.budget, solution);
+    cout << "Best gifts for " << person << ":" << endl;
+    show(solution, solution.budget);
+    cout << endl;
+
+    out << "Best gifts for " << person << ":" << endl;
+    for (Gift gift : solution.gifts) {
+        out << gift;
+        out << endl;
+    }
+    out << "Budget remaining: " << solution.budget << " cents" << endl;
+    out << endl;
 }
 
 #ifndef TESTING
@@ -106,16 +190,13 @@ int main ()
 /*  Postcondition: the gift store has been loaded, and the optimal gift lists of each name
     (Andrew ... Fabienne) has been computed and printed to `cout`
 */
-    Gift test;
-    ifstream store("giftstore.txt");
-    Store giftstore;
-    read_store(store, giftstore);
-    cout << giftstore;
-    ifstream Andrew("Andrew.txt");
-    Wishlist list;
-    read_available_wishlist(Andrew, list, giftstore);
-    show(list, list.budget);
-
+    ofstream best("best_gifts.txt");
+    calculate_wishlist("Andrew","giftstore", best);
+    calculate_wishlist("Belle","giftstore", best);
+    calculate_wishlist("Chris","giftstore", best);
+    calculate_wishlist("Desiree","giftstore", best);
+    calculate_wishlist("Edward","giftstore", best);
+    calculate_wishlist("Fabienne","giftstore", best);
 
     return 0;
 }
